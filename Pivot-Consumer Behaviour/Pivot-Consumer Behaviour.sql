5382c1d2862d82a39406b658ab6ed5284fb4efd7
An Duong <122582698+Duongxuanan@users.noreply.github.com>
2023-03-15 22:42:25 +0700

    Add files via upload

---
+++ Pivot-Consumer Behaviour/Pivot-Consumer Behaviour.sql
@@ -0,0 +1,25 @@
+-- Consumer Behaviour
+SELECT
+	a.CustomerID
+	,a.SalesOrderID
+	,e.Name ProductType
+	,c.ProductLine
+	,c.ProductID
+--INTO pv04
+FROM Sales.SalesOrderHeader a INNER JOIN Sales.SalesOrderDetail b ON a.SalesOrderID = b.SalesOrderID
+INNER JOIN Production.Product c ON b.ProductID = c.ProductID
+INNER JOIN Production.ProductSubcategory d ON c.ProductSubcategoryID = d.ProductSubcategoryID
+INNER JOIN Production.ProductCategory e ON d.ProductCategoryID = e.ProductCategoryID
+
+
+SELECT * 
+--INTO pv05
+FROM
+	(SELECT DISTINCT SalesOrderID, ProductType, cnt = 1 FROM pv04) a
+PIVOT(COUNT (Cnt) FOR ProductType IN ([Bikes], [Accessories] ,[Clothing], [Components])
+) b
+
+SELECT Bikes ,Accessories ,Clothing ,Components ,COUNT(*) num_orders
+FROM pv05
+GROUP BY Bikes, Accessories, Clothing, Components
+ORDER BY num_orders DESC
