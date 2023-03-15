e19888eebc4a3d2c87d6777135e3520f9fff0c7b
An Duong <122582698+Duongxuanan@users.noreply.github.com>
2023-03-15 22:45:34 +0700

    Update Pivot-Consumer Behaviour.sql

--- Pivot-Consumer Behaviour/Pivot-Consumer Behaviour.sql
+++ Pivot-Consumer Behaviour/Pivot-Consumer Behaviour.sql
@@ -1,10 +1,10 @@
 -- Consumer Behaviour
 SELECT
-	a.CustomerID
-	,a.SalesOrderID
-	,e.Name ProductType
-	,c.ProductLine
-	,c.ProductID
+a.CustomerID,
+a.SalesOrderID,
+e.Name ProductType,
+c.ProductLine,
+c.ProductID
 --INTO pv04
 FROM Sales.SalesOrderHeader a INNER JOIN Sales.SalesOrderDetail b ON a.SalesOrderID = b.SalesOrderID
 INNER JOIN Production.Product c ON b.ProductID = c.ProductID
@@ -16,10 +16,14 @@ SELECT *
 --INTO pv05
 FROM
 	(SELECT DISTINCT SalesOrderID, ProductType, cnt = 1 FROM pv04) a
-PIVOT(COUNT (Cnt) FOR ProductType IN ([Bikes], [Accessories] ,[Clothing], [Components])
-) b
+PIVOT(COUNT (Cnt) FOR ProductType IN ([Bikes], [Accessories] ,[Clothing], [Components])) b
 
-SELECT Bikes ,Accessories ,Clothing ,Components ,COUNT(*) num_orders
+SELECT 
+Bikes,
+Accessories,
+Clothing,
+Components,
+COUNT(*) num_orders
 FROM pv05
 GROUP BY Bikes, Accessories, Clothing, Components
 ORDER BY num_orders DESC
